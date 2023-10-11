f1e3cf2fa587d91c673afd75af03bb62d70cc3c5
Felipe Sutter <lipeps61@gmail.com>
2023-10-11 20:11:41 -0300

    Criação da entidade Instrutor

---
+++ academia/src/main/java/com/residencia/academia/entities/Instrutor.java
@@ -0,0 +1,66 @@
+package com.residencia.academia.entities;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "codigoInstrutor", scope=Instrutor.class)
+@Entity
+@Table(name = "instrutor")
+public class Instrutor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigoinstrutor")
+	private Integer codigoInstrutor;
+	
+	@Column(name = "rg", unique = true)
+	private Long rg;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+//	@OneToOne(mappedBy = "instrutor")
+//	private Telefone telefone;
+	
+	// colocar 1:n de instrutor p/ turma, fazer um list
+	
+	public Integer getCodigoInstrutor() {
+		return codigoInstrutor;
+	}
+	
+	public void setCodigoInstrutor(Integer codigoInstrutor) {
+		this.codigoInstrutor = codigoInstrutor;
+	}
+	
+	public Long getRg() {
+		return rg;
+	}
+	
+	public void setRg(Long rg) {
+		this.rg = rg;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+//	public Telefone getTelefone() {
+//		return telefone;
+//	}
+//
+//	public void setTelefone(Telefone telefone) {
+//		this.telefone = telefone;
+//	}
+	
+}
